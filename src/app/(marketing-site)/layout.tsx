dc23f01b821461d2e5215bbbb4694f54bf9389a7
sharqawycs <sharqawycs@gmail.com>
2025-10-10 05:27:05 +0300

    Refactor: remove unused Footer import from marketing site layout

--- src/app/(marketing-site)/layout.tsx
+++ src/app/(marketing-site)/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import Footer from '@/components/features/footer/Footer';
 import StructuredData from '@/components/seo/StructuredData';
 import './marketing.css';
 
