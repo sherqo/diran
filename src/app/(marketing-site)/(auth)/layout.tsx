2b2ed00e31383b35a84862ef9a653420ab4b366e
sharqawycs <sharqawycs@gmail.com>
2025-10-10 06:29:15 +0300

    Refactor code structure for improved readability and maintainability

--- src/app/(marketing-site)/(auth)/layout.tsx
+++ src/app/(marketing-site)/(auth)/layout.tsx
@@ -1,4 +1,8 @@
 import type { Metadata } from 'next';
+import Image from 'next/image';
+import logo from '@/imgs/logo-192.png';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
     title: 'Diran AI',
@@ -11,7 +15,13 @@ export default function AuthLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+        <div className="bg-background container mx-auto flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <Button variant="ghost" className="absolute top-2 left-4 flex items-center gap-2 rounded-lg px-2">
+                <Link href="/" className="flex items-center gap-2">
+                    <Image src="/identity/logo-192.png" alt="Diran AI" width={30} height={30} />
+                    <span className="font-clash font-lg">Diran AI</span>
+                </Link>
+            </Button>
             <div className="w-full max-w-sm">{children}</div>
         </div>
     );
