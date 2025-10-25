bf60abcfd116574b3f9588ed7ecc181d641c8d80
sharqawycs <sharqawycs@gmail.com>
2025-10-25 13:36:27 +0300

    feat(schema): add Block type definitions and response interfaces

---
+++ packages/shared/src/types/block.ts
@@ -0,0 +1,43 @@
+export const enum BlockType {
+  PAGE = 'PAGE',
+
+  // Headings
+  HEADING_1 = 'HEADING_1',
+  HEADING_2 = 'HEADING_2',
+  HEADING_3 = 'HEADING_3',
+
+  PARAGRAPH = 'PARAGRAPH',
+  CODE = 'CODE',
+  TODO = 'TODO',
+}
+
+// we can later extend make this `Block<T>` and have `T` be the content type
+export interface Block {
+  // All of this should be synced with the database model
+  id: string;
+  type: BlockType;
+  parentId?: string;
+  order: number;
+  content: Record<string, any>; // JSON content varies by block type
+  createdAt: string;
+  updatedAt: string;
+  children?: Block[];
+}
+
+export interface CreateBlockResponseData {
+  block: Block; // do i really need to return it????
+}
+
+export interface GetBlockResponseData {
+  block: Block;
+}
+
+export interface UpdateBlockResponseData {
+  block: Block; // do i really need to return it????
+}
+
+export interface DeleteBlockResponseData {}
+
+export interface GetBlockChildrenResponseData {
+  blocks: Block[];
+}
