d643431098fae212186c338121ee3a99d522f17c
Sherqo <sharqawycs@gmail.com>
2025-11-24 20:33:43 +0200

    editor-js step-0

--- apps/frontend/src/app/(app)/home/page.tsx
+++ apps/frontend/src/app/(app)/home/page.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import { AppSidebar } from '@/components/app-sidebar';
 import { NavActions } from '@/components/nav-actions';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage } from '@/components/ui/breadcrumb';
 import { Separator } from '@/components/ui/separator';
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
 
 const PageHeader = ({ className }: { className?: string }) => (
     <header className={`bg-sidebar flex h-14 shrink-0 items-center gap-2 border-b ${className || ''}`}>
@@ -23,11 +29,49 @@ const PageHeader = ({ className }: { className?: string }) => (
     </header>
 );
 
-const PageBody = ({ className }: { className?: string }) => (
-    <div className={`flex flex-col gap-4 px-4 py-10 ${className || ''}`}>{/* Content will go here */}</div>
-);
+const PageBody = ({ className }: { className?: string }) => {
+    return (
+        <div className={`flex flex-col gap-4 px-4 py-10 ${className || ''}`}>
+            {/* Content will go here */}
+            this editor should be her <div id="editorjs" className="w-full max-w-3xl" />
+        </div>
+    );
+};
 
 export default function Page() {
+    const editor = new EditorJS({
+        /**
+         * Id of Element that should contain the Editor
+         */
+        holder: 'editorjs',
+
+        /**
+         * Available Tools list.
+         * Pass Tool's class or Settings object for each Tool you want to use
+         */
+        tools: {
+            header: Header,
+            list: List,
+        },
+
+        onReady: () => {
+            console.log('Editor.js is ready to work!');
+        },
+
+        onChange: (api, event) => {
+            console.log("Now I know that Editor's content changed!", event);
+        },
+    });
+
+    editor.isReady
+        .then(() => {
+            console.log('Editor.js is ready to work!');
+            /** Do anything you need after editor initialization */
+        })
+        .catch(reason => {
+            console.log(`Editor.js initialization failed because of ${reason}`);
+        });
+
     return (
         <SidebarProvider>
             <AppSidebar />
