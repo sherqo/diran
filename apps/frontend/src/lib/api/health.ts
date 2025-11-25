393abe746d3f09f5de5b2c681dc58e2e452175a8
Sherqo <sharqawycs@gmail.com>
2025-11-25 17:04:40 +0200

    delete unused file `health.ts`

--- apps/frontend/src/lib/api/health.ts
+++
@@ -1,4 +0,0 @@
-import { apiRequest } from './helpers';
-import type { GetHealthResponseData } from '@/shared/types/health';
-
-export const getHealthApi = () => apiRequest<GetHealthResponseData>('/health');
