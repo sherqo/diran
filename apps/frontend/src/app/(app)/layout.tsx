c0ffa53cbde96a7c0a357d7428dd474cea9674db
Sherqo <sharqawycs@gmail.com>
2025-11-28 02:17:28 +0200

    remove the breaking change and remove the 'use client'

--- apps/frontend/src/app/(app)/layout.tsx
+++ apps/frontend/src/app/(app)/layout.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { ProtectedRoute } from '@/components/auth/ProtectedRoute';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { PageProvider } from '@/contexts/PageContext';
